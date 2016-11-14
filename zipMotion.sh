740c29d17130b759de6262a0bb814606c1dc4afb
pespringer <patrick.springer@gmail.com>
2016-11-13 23:40:37 -0500

    version 3 changes

--- zipMotion.sh
+++ zipMotion.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
 shopt -s nullglob
-dt=$(date --date yesterday "+%Y%m%d")
-#dt=20161021
+#dt=$(date --date yesterday "+%Y%m%d")
+dt=20161110
 srcDir=/var/lib/motion
 destDir=~/motionBkup/
 tarFile=${dt}MotionFiles.tar
 tempFile=/tmp/tempList.txt
-arr=(${srcDir}/*${dt}*)
-
 
 echo "GENERATING LIST OF FILES TO TAR..."
+arr=(${srcDir}/*${dt}*)
+
+echo "${#arr[@]} total files to be dealt with..."
+tar -cf /tmp/${tarFile} --files-from /dev/null
+sleep 5
+
 for ((i=0; i<${#arr[@]}; i++)); 
 do
-    echo "${arr[$i]}"
+    echo "Dealing with file number $i of ${#arr[@]} ..."
+    f=${arr[$i]}
+    tar -rf /tmp/${tarFile} -P $f 2>&1 >> /dev/null
+    sudo rm -rf $f
 done
+
+echo "TARING UP FILE..."
+gzip /tmp/${tarFile}
+echo "MOVING TAR FILE..."
+echo "sudo mv /tmp/${tarFile}.gz ${destDir}"
+
+exit
+
+
+
+
+
+
 
 echo "TARING UP MOTION FILES FROM ${dt}...."
 tar -cf /tmp/${tarFile} --files-from /dev/null
 
 for f in "${arr[@]}"
 do
-  echo $f
-  tar -rf /tmp/${tarFile} $f
+  #echo $f
+  tar -rf /tmp/${tarFile} $f 
 done
 gzip /tmp/${tarFile}
 
@@ -32,36 +52,6 @@ sudo mv /tmp/${tarFile}.gz ${destDir}
 echo "DELETING TARED FILES..."
 for f in "${arr[@]}"
 do
-  echo "REMOVING FILE $f...."
-  sudo rm -rf $f
+  echo "REMOVING FILE $f...." 
+  sudo rm -rf $f 
 done
-
-exit
-
-
-
-
-
-
-echo $dt
-#ls ${dir}/*${dt}*
-
-echo "GENERATING LIST OF FILES TO TAR..."
-ls -lart ${srcDir}/*${dt}* | awk '{print $9}' >> ${tempFile}
-
-echo "TARING UP MOTION FILES FROM ${dt}...."
-#echo sudo tar czvf "${tarFile}" -T "${srcDir}/*${dt}*"
-tar -czvf ${tarFile} -T ${tempFile}
-
-echo "MOVING TARED FILE TO ${destDir}..."
-sudo mv ${tarFile} ${destDir}
-
-echo "DELETING TARED FILES..."
-for i in `grep ^ ${tempFile}`
-do
-  echo "Removing file $i..."
-  sudo rm -rf $i
-done
-
-echo "CLEANING UP TEMP INFO..."
-sudo rm -f ${tempFile}
