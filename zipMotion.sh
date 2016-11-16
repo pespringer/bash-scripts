115941cbfa0eabe90edd9db72cac0b4f924abc5f
pespringer <patrick.springer@gmail.com>
2016-11-15 23:39:53 -0500

    Added initial logging, log viewer, meh test function, worker function should also run in background

--- zipMotion.sh
+++ zipMotion.sh
@@ -8,21 +8,38 @@ prevDay=$(date --date yesterday "+%Y%m%d")
 RED='\033[0;41;30m'
 STD='\033[0;0;39m'
 
+echo "Starting menu script at `date`" 2>&1 >> /home/pi/worker.log
+
 # user defined functions
+meh(){
+    while true
+    do
+        echo "running..." 2>&1 >> /home/pi/worker.log
+        sleep 1
+    done &
+    echo ""
+    read -p "Job is running in background.  Press [Enter] to continue..." fackEnterKey
+}
+
 worker() {
+    echo "Starting worker function at `date`" 2>&1 >> /home/pi/worker.log
     tarFile=${dt}MotionFiles.tar
     tar -cf /tmp/${tarFile} --files-from /dev/null
     for ((i=0; i<${#arr[@]}; i++)); 
     do
         fileNum=$((i + 1))
-        echo "Dealing with file number $fileNum of ${#arr[@]} ..."
+        echo "Dealing with file number $fileNum of ${#arr[@]} ..." 2>&1 >> /home/pi/worker.log
         f=${arr[$i]}
         tar -rf /tmp/${tarFile} -P $f 2>&1 >> /dev/null
-        sudo rm -rf $f
-    done
+        sudo rm -rf $f 2>&1 >> /home/pi/worker.log
+    done &
+    echo ""
+    read -p "Job is running in background.  Press [Enter] to continue..." fackEnterKey
+    #exit 1
 }
 
 cleanup() {
+    echo "Starting cleanup function at `date`" 2>&1 >> /home/pi/worker.log
     tarFile=${dt}MotionFiles.tar
     echo "TARING UP FILE..."
     gzip /tmp/${tarFile}
@@ -31,6 +48,7 @@ cleanup() {
 }
 
 pause(){
+    echo ""
     read -p "Job complete, press [Enter] key to continue..." fackEnterKey
 }
 
@@ -45,8 +63,10 @@ one(){
         pause
     else
         echo "${#arr[@]} total files to be dealt with..."
+        echo "${#arr[@]} FILES TO BE DEALT WITH..." 2>&1 >> /home/pi/worker.log
         sleep 5
         worker
+        wait #Wait for worker function to complete before cleanup moves tar file
         cleanup
         pause
     fi      
@@ -64,6 +84,7 @@ two(){
         pause
     else
         echo "${#arr[@]} total files to be dealt with..."
+        echo "${#arr[@]} FILES TO BE DEALT WITH..." 2>&1 >> /home/pi/worker.log
         sleep 5
         worker
         cleanup
@@ -71,6 +92,20 @@ two(){
     fi      
 }
 
+three() {
+    jobs
+    if ((`jobs |wc -l` == 0))
+    then
+        echo "No jobs are running"
+    fi
+    pause
+}
+
+four() {
+    tail -n100 /home/pi/worker.log
+    echo ""
+    read -p "Job is running in background.  Press [Enter] to continue..." fackEnterKey
+}
 
 # function to display menu
 show_menus() {
@@ -80,16 +115,20 @@ show_menus() {
     echo "~~~~~~~~~~~~~~~~~"
     echo "1. Run for previous day ${prevDay}"
     echo "2. Enter date for run"
-    echo "3. EXIT"
+    echo "3. See Running Job"
+    echo "4. View Log File"
+    echo "q  EXIT"
 }
 
 read_options(){
     local choice
-    read -p "Enter choice [ 1 - 3 ] " choice
+    read -p "Enter choice [ 1 - 4 / q ] " choice
     case ${choice} in
         1) one ;;
         2) two ;;
-        3) exit 0;;
+        3) three ;;
+        4) four ;;
+        q) exit 0;;
         *) echo -e "${RED}Error...${STD}" && sleep 2
     esac
 }
